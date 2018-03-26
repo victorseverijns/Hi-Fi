de4c936e789ef012020b2f83786674d2015fc3f8
Victor Severijns <info@victorseverijns.nl>
2018-03-26 20:11:19 +0200

    Basis
    Basis locatieoverzicht, lettertypes en bootstrap

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Locatieoverzicht #A - UMC Utrecht</title>
+		<link rel="stylesheet" type="text/css" href="fa-regular.css" />
+		<link rel="stylesheet" type="text/css" href="fa-solid.css" />
+		<link rel="stylesheet" type="text/css" href="fontawesome.css" />
+		<link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheets/overzicht.css" />
+	</head>
+	<body>
+		<div id="header">
+			<div id="personal">
+				<img src="images/user.png" /> <span>T. Kappen</span>
+			</div>
+			<div id="hulp">
+
+			</div>
+			<div id="eigenoks">
+				<div class="eigenok"></div>
+				<div class="eigenok"></div>
+			</div>
+		</div>
+		<div id="overzicht">
+
+		</div>
+	</body>
+</html>
