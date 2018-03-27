5fbdb93b3ca7a90cd55d4935fb11aa6e36680fb8
danavandelden <danavd1998@gmail.com>
2018-03-27 15:17:53 +0200

    positie hulp

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 			</div>
 							  
 			<div id="hulp">
-				<span> <CENTER>! HßLP <br></br>  <br></br>  <br></br>  <br></br>  <br></br></CENTER> </span>
+				<span> <CENTER>! HULP <br></br>  <br></br>  <br></br>  <br></br>  <br></br></CENTER> </span>
 
 			</div>
 			<div id="eigenoks">
