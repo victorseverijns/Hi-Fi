9d6532cf829e8dd3b25997a9b6ad45d913a55a7d
Victor Severijns <info@victorseverijns.nl>
2018-03-27 11:33:07 +0200

    Patientview basis

---
+++ patientview.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Patiënt view</title>
+		<link rel="stylesheet" type="text/css" href="stylesheets/fa-regular.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheets/fa-solid.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheets/fontawesome.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheets/patientview.css" />
+	</head>
+	<body>
+		<div class="row">
+			<div id="leftSide" class="col-md-3">
+
+			</div>
+			<div id="rightSide" class="col-md-9">
+
+			</div>
+		</div>
+	</body>
+</html>
