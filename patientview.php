dca9ee210224ce0ba78a102bb9a1332438e63c93
Victor Severijns <info@victorseverijns.nl>
2018-03-27 13:20:09 +0200

    Patiëntview

--- patientview.php
+++ patientview.php
@@ -9,9 +9,31 @@
 		<link rel="stylesheet" type="text/css" href="stylesheets/patientview.css" />
 	</head>
 	<body>
-		<div class="row">
+		<div id="view" class="row">
 			<div id="leftSide" class="col-md-3">
+				<div id="ok">
+					<span id="back" class="fa fa-arrow-circle-left"></span> OK #1 <span id="important" class="fa fa-exclamation-circle"></span>
+					<div id="bar">
+						<span>18:00u</span>
+						<div class="progress">
+							<div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+								70%
+							</div>
+						</div>
+					</div>
+				</div>
+				<div id="patient">
+					J. Janssen <span class="fa fa-user"></span><br />
+					57 jaar<br />
+					Appendectomie
+				</div>
+				<div id="medewerkers">
+					<span>T. Kappen</span><br />
+					J. Hendriks
+				</div>
+				<div id="meldingcontainer">
 
+				</div>
 			</div>
 			<div id="rightSide" class="col-md-9">
 
