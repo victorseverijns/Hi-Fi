19a1e6a938833addc07aca93823c41a50e382721
danavandelden <danavd1998@gmail.com>
2018-03-28 18:29:20 +0200

    woord operatie

--- index.php
+++ index.php
@@ -28,32 +28,32 @@
 
 		<div id="overzicht" class="row">
 				 <div class="col-md-2" id="arts1" ><img src="images/user.png"/> <span>&nbsp Adams</span> 
-							 <div id="ok3"> <span> OK #3 </span> </div> 
-							 <div id="ok5"> <span> OK #5 </span> </div>
+							 <div id="ok3"> <span> OK #3 <break> Operatie </span> </div> 
+							 <div id="ok5"> <span> OK #5 <break> Operatie </span> </div>
 						
 				</div>
 			
 				<div class="col-md-2" id="arts2" ><img src="images/user.png"/> <span> &nbsp Gillisen </span> 
-							<div id="ok2"> <span> OK #2 </span> </div>
-							<div id="ok9"> <span> OK #9 </span> </div>
+							<div id="ok2"> <span> OK #2 </span> <break> Operatie</div>
+							<div id="ok9"> <span> OK #9 </span> <break> Operatie </div>
 				</div>
 		
 				<div class="col-md-2" id="arts3" ><img src="images/user.png"/> <span> &nbsp Haringa </span>
-							<div id="ok10"> <span> OK #10 </span> </div>
-							<div id="ok11"> <span> OK #11 </span> </div>
+							<div id="ok10"> <span> OK #10 </span> <break> Operatie  </div>
+							<div id="ok11"> <span> OK #11 </span> <break> Operatie </div>
 				</div>
 				   
 				<div class="col-md-2" id="arts4" ><img src="images/user.png"/> <span> &nbsp Janssen </span>
-							<div id="ok12"> <span> OK #12 </span> </div> 
+							<div id="ok12"> <span> OK #12 </span> <break> Operatie </div> 
 				</div>
 				
-				<div class="col-md-2" id="arts5" ><img src="images/user.png"/> <span> &nbsp Kappen </span>
+				<div class="col-md-2" id="arts5" ><img src="images/user.png"/> <span> &nbsp Kappen <break> <break> <break> <break> Momenteel geen Operatie </span>
 				</div>
 		
 				
 				<div class="col-md-2" id="arts6" ><img src="images/user.png"/> <span> &nbsp Zwet </span> 
-							<div id="ok1"> <span> OK #1 </span> </div>
-							<div id="ok6"> <span> OK #6 </span> </div> 
+							<div id="ok1"> <span> OK #1 </span> <break> Operatie </div>
+							<div id="ok6"> <span> OK #6 </span> <break> Operatie </div> 
 				</div>
 		</div>
 
