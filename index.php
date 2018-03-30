9570f9d07c4ad7c0fed777606d26c1a106e0534b
danavandelden <danavd1998@gmail.com>
2018-03-30 15:43:49 +0200

    input

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 				<span> <CENTER> <FONT size="4"> OK #3 </FONT> <br> <FONT size="2"> Operatie </FONT> </CENTER>  </span> 
 					</span>
 				<div id="hulpinput"> 
-				<span>  &#9989 &nbsp &#10060 
+				<span>  &#9989 &#10060 
 					 <span> 
 				</div>
 
